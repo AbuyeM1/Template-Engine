18b0a05a39e45b4fae2bcbfe0520839072b77c27
Abuye Mamuye <abuyeye3@gmail.com>
2021-01-20 15:35:09 -0800

    add class on the code

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email, title){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.title = title;
+    }
+    getName() {
+        console.log(this.name);
+        return this.name;
+    }
+    getEmail() {
+        // console.log(this.email);
+        return this.email;
+    }
+    getTitle() {
+        console.log(this.title);
+        return this.title;
+    }
+}
+module.exports = Employee
